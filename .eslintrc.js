50babae40e688a4913dbc94b023d007aa94eda83
Jake Deichert <git@jakedeichert.com>
2019-10-04 00:17:35 -0400

    Set up typescript eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
     extends: '@jakedeichert/eslint-config',
+
+    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+
+    rules: {
+        // Disable things prettier handles
+        '@typescript-eslint/indent': 'off',
+    },
 };
