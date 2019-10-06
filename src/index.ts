11adfe0ad371f7c8cd231932765bd50374e5fec3
Jake Deichert <git@jakedeichert.com>
2019-10-05 23:06:04 -0400

    Refactor into a builder pattern

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,56 @@
-export const repeatPromise = (
-    promiseFunc: () => Promise<any>,
-    maxAttempts: number,
-    sleepMs: number = 0
-): any => {
-    const attempt = (attemptCount: number) => {
-        return new Promise((resolve, reject) => {
-            promiseFunc()
-                .then(result => {
-                    resolve(result);
-                })
-                .catch(rejectedValue => {
-                    if (attemptCount === maxAttempts) {
-                        return reject(rejectedValue);
-                    }
-                    // Failed attempt. Try again after sleepMs
-                    setTimeout(() => {
-                        resolve(attempt(attemptCount + 1));
-                    }, sleepMs);
-                });
-        });
+/* eslint-disable @typescript-eslint/no-explicit-any */
+interface RepeatBuilder {
+    maxAttempts(attempts: number): RepeatBuilder;
+    delay(delayBetweenAttemptsMs: number): RepeatBuilder;
+    start(): Promise<any>;
+}
+
+export function repeat(promiseFunc: () => Promise<any>): RepeatBuilder {
+    const config = {
+        maxAttempts: 0,
+        delayBetweenAttemptsMs: 0,
     };
-    return attempt(1);
-};
+
+    const state = {
+        attempts: 0,
+    };
+
+    const self: RepeatBuilder = {
+        maxAttempts(attempts: number): RepeatBuilder {
+            config.maxAttempts = attempts;
+            return this;
+        },
+        delay(delayBetweenAttemptsMs: number): RepeatBuilder {
+            config.delayBetweenAttemptsMs = delayBetweenAttemptsMs;
+            return this;
+        },
+        start(): Promise<any> {
+            return attempt();
+        },
+    };
+
+    async function attempt(): Promise<any> {
+        state.attempts++;
+        let result;
+        try {
+            result = await promiseFunc();
+        } catch (error) {
+            if (state.attempts === config.maxAttempts) {
+                throw error;
+            }
+            // Failed attempt. Delay and then try again.
+            await sleep(config.delayBetweenAttemptsMs);
+            result = await attempt();
+        }
+        return result;
+    }
+    return self;
+}
+
+function sleep(timeoutMs: number): Promise<void> {
+    return new Promise((resolve): void => {
+        setTimeout(() => {
+            resolve();
+        }, timeoutMs);
+    });
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import { repeatPromise } from '@src/index';
+import { repeat } from '@src/index';
 import * as mockTimers from '@test/utils/mockTimers';
 
 beforeEach(() => {
@@ -11,25 +11,29 @@ afterEach(() => {
 
 describe('repeatPromise', () => {
     test('should resolve to the value of the promise', async () => {
-        const promiseFunc = () => {
-            return new Promise(resolve => {
+        const promiseFunc = (): Promise<string> => {
+            return new Promise((resolve): void => {
                 resolve('RETURN_VALUE');
             });
         };
-        const val = await repeatPromise(promiseFunc, 1);
+        const val = await repeat(promiseFunc)
+            .maxAttempts(1)
+            .start();
         expect(val).toBe('RETURN_VALUE');
     });
 
     test('should reject when max attempts have been reached', async () => {
         let rejectedValue = null;
-        const promiseFunc = () => {
-            return new Promise((_, reject) => {
+        const promiseFunc = (): Promise<void> => {
+            return new Promise((_, reject): void => {
                 reject('FAILED');
             });
         };
 
         try {
-            await repeatPromise(promiseFunc, 1);
+            await repeat(promiseFunc)
+                .maxAttempts(1)
+                .start();
         } catch (e) {
             rejectedValue = e;
         }
@@ -39,15 +43,17 @@ describe('repeatPromise', () => {
     test('should reject after the correct amount of max attempts', async () => {
         let counter = 0;
         let rejectedValue = null;
-        const promiseFunc = () => {
+        const promiseFunc = (): Promise<void> => {
             counter++;
-            return new Promise((_, reject) => {
+            return new Promise((_, reject): void => {
                 reject('FAILED');
             });
         };
 
         try {
-            await repeatPromise(promiseFunc, 4);
+            await repeat(promiseFunc)
+                .maxAttempts(4)
+                .start();
         } catch (e) {
             rejectedValue = e;
         }
@@ -59,16 +65,18 @@ describe('repeatPromise', () => {
         let counter = 0;
         let err = null;
         let value = null;
-        const promiseFunc = () => {
+        const promiseFunc = (): Promise<string> => {
             counter++;
-            return new Promise((resolve, reject) => {
+            return new Promise((resolve, reject): void => {
                 if (counter === 3) return resolve('RETURN_VALUE');
                 reject('FAILED');
             });
         };
 
         try {
-            value = await repeatPromise(promiseFunc, 4);
+            value = await repeat(promiseFunc)
+                .maxAttempts(4)
+                .start();
         } catch (e) {
             err = e;
         }
@@ -78,23 +86,26 @@ describe('repeatPromise', () => {
     });
 
     test('should obey the sleep timeout option', async () => {
-        // @ts-ignore
         global.setTimeout = jest.fn((cb, ms) => {
             expect(ms).toBe(100);
             expect(cb).toEqual(expect.any(Function));
             cb();
-        });
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        }) as any;
 
         let counter = 0;
-        const promiseFunc = () => {
+        const promiseFunc = async (): Promise<string> => {
             counter++;
-            return new Promise((resolve, reject) => {
-                if (counter === 3) return resolve('RETURN_VALUE');
-                reject('FAILED');
-            });
+            if (counter === 3) {
+                return 'RETURN_VALUE';
+            }
+            throw 'FAILED';
         };
 
-        const val = await repeatPromise(promiseFunc, 4, 100);
+        const val = await repeat(promiseFunc)
+            .maxAttempts(4)
+            .delay(100)
+            .start();
         expect(val).toBe('RETURN_VALUE');
         expect(global.setTimeout).toHaveBeenCalledTimes(2);
     });
